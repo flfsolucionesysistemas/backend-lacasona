598c5de606ff1d704d2fad38e407c4297722a2bc
cristian f <licagua@gmail.com>
2020-11-11 21:25:34 +0000

    cambio controlador buscar turnos entrevista libre

--- backend-lacasona/controllers/turno.js
+++ backend-lacasona/controllers/turno.js
@@ -5,7 +5,8 @@ const helpers = require('../config/helpers');
 
 exports.getTrunosEntrevista = async (req, res) =>{
     
-	let body = await pool.query ('SELECT * FROM turno WHERE turno_tratamiento = 0');
+	let body = await pool.query ('SELECT * FROM turno WHERE turno_tratamiento = 0 and estado = 1');
+	//and fecha >= now();
 	
     if(body != null){
         res.status(200).send({body});      
