3894db12f91bf4b07841241a0f746c9e49bebce4
cristian f <licagua@gmail.com>
2020-11-17 02:46:29 +0000

    modificacion getUserId, quite el join con localidad

--- backend-lacasona/controllers/users.js
+++ backend-lacasona/controllers/users.js
@@ -160,8 +160,8 @@ exports.getUserId= async (req, res) =>{
     let valor = req.params.idUser;
     console.log(req.params.idUser);
     
-    let body = await pool.query ('SELECT * FROM persona join localidad on localidad.id_localidad=persona.id_localidad WHERE id_persona = '+valor);
-	
+    let body = await pool.query ('SELECT * FROM persona WHERE id_persona = '+valor);
+	//let body = await pool.query ('SELECT * FROM persona join localidad on localidad.id_localidad=persona.id_localidad WHERE id_persona = '+valor);
     if(body != null){
         res.status(200).send({body});
        
