48bed000d81c2ffa2f9b2f6d78858bb5e3d4d732
cristian f <licagua@gmail.com>
2020-11-28 14:26:24 +0000

    api tratamiento

--- backend-lacasona/controllers/tratamiento.js
+++ backend-lacasona/controllers/tratamiento.js
@@ -16,3 +16,65 @@ exports.add=async(req,res)=>{
         });
           
 }
+
+exports.updateTratamiento = async(req,res)=>{
+    let data = req.body;
+    await pool.query('UPDATE tratamiento SET ? WHERE id_tratamiento = ?', [data,data.id_tratamiento], function(err,sql){
+        if(err){
+            console.log(err);
+            res.status(400).json({
+                mensaje: 'Ocurrio un problema al intentar modificar'
+            });
+        }
+        else{
+            console.log(sql);
+            res.status(200).json({
+            mensaje: 'El tratamiento se modifico con exito'
+            }); 
+        }
+    });
+}
+exports.listaTratamietos=async(req,res)=>{
+    await pool.query('SELECT * FROM tratamiento', function(err, lista_tratamiento){
+        if (err) {
+            res.json({
+                resultado: false,
+                mensaje: 'No se pudieron listar los tratamientos',
+                err
+            });
+        } else {
+            res.json({
+                resultado: true,
+                mensaje: 'los tratamientos se listaron adecuadamente',
+                lista_tratamiento
+            });
+        }
+    });
+}
+
+exports.listaTratamietosActivos=async(req,res)=>{
+    let valor = req.params.valor ;
+    if(valor== 0||valor ==1){ 
+       await pool.query('SELECT * FROM tratamiento where activo = ?',[valor] , function(err, lista_tratamientos){
+        if (err) {
+            res.json({
+                resultado: false,
+                mensaje: 'No se pudieron listar los tratamientos',
+                err
+            });
+        } else {
+            res.json({
+                resultado: true,
+                mensaje: 'Los tratamientos se listaron adecuadamente',
+                lista_tratamientos
+            });
+        }
+      
+      });
+    }else {
+    res.json({
+        resultado: false,
+        mensaje: 'El valor no corresponde a un parametro valido'
+    });
+}
+}
--- backend-lacasona/router/tratamiento.js
+++ backend-lacasona/router/tratamiento.js
@@ -3,4 +3,7 @@ var router = express.Router();
 var ctrlTratamiento = require('../controllers/tratamiento');
 
 router.post('/add',ctrlTratamiento.add);
+router.put('/update',ctrlTratamiento.updateTratamiento);
+router.get('/listar',ctrlTratamiento.listaTratamientos);
+router.get('/listarActivos/:valor',ctrlTratamiento.listaTratamientosActivos);
 module.exports = router;
