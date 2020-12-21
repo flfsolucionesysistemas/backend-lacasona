d5b86966ffd65567bbd0097e37986bad4845f5ee
cristian f <licagua@gmail.com>
2020-12-21 19:12:57 +0000

    .

--- backend-lacasona/controllers/turno.js
+++ backend-lacasona/controllers/turno.js
@@ -78,7 +78,7 @@ exports.getTurnosDisponiblesTipoTodos = async (req, res) =>{
 			}); 
 		}
 	}else{
-		let body = await pool.query ('SELECT t.fecha,t.hora, t.estado, t.costo_base, t.observacion, t.turno_tratamiento, t.id_profesional, p.nombre, p.apellido ' +
+		let body = await pool.query ('SELECT t.fecha,t.hora, t.estado, t.costo_base, t.observacion, t.turno_tratamiento, t.id_profesional, t.profesional_disponible , p.nombre, p.apellido ' +
 				' FROM turno as t ' +
 				' inner join persona as p on p.id_persona = t.id_profesional ' +
 				' WHERE t.turno_tratamiento = 1 and (t.fecha > CURDATE() OR  (t.fecha = CURDATE() and t.hora >= DATE_FORMAT(NOW( ), "%H:%i:%S"))) ' +
