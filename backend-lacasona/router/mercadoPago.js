9b6fe2bb78e506a684bf678b5b2e95edf0bac815
unknown <analia.f93@gmail.com>
2020-12-29 18:57:44 -0300

    mercado pago

--- backend-lacasona/router/mercadoPago.js
+++ backend-lacasona/router/mercadoPago.js
@@ -3,6 +3,6 @@ var router = express.Router();
 var ctrlMP = require('../controllers/mercadopago');
 
 router.post('/obtenerurl',ctrlMP.obtenerUrlDePago);
-router.post('/notificacion/:nombre/:apellido/:email/:telefono/:localidadSleccionada/:turnoSeleccionado/:costo_entrevista', ctrlMP.confirmarPago);
+router.post('/notificacion/:nombre/:apellido/:email/:telefono/:id_localidad/:id_turno/:costo_entrevista', ctrlMP.confirmarPago);
 
 module.exports = router;
