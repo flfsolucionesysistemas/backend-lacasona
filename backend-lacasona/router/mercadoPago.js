20983ba8ff662fdd966941cf4dd4eb7b79faafbd
unknown <analia.f93@gmail.com>
2021-01-22 18:36:39 -0300

    nuevo metodo

--- backend-lacasona/router/mercadoPago.js
+++ backend-lacasona/router/mercadoPago.js
@@ -7,5 +7,6 @@ router.post('/notificacion/:nombre/:apellido/:email/:telefono/:id_localidad/:id_
 router.post('/notificacionTratamiento/:id_hc/:id_cupon', ctrlMP.confirmarPagoTratamiento);
 router.post('/obtenerurlTratamiento',ctrlMP.obtenerUrlDePagoTratamiento);
 router.get('/getCuponesPaciente/:id_paciente',ctrlMP.getCuponesPacientes);
+router.get('/getPagoTipo/:tipo',ctrlMP.getCuponTipo);
 
 module.exports = router;
