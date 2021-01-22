2d80eb83752dae0322b98b68299c900aff2bf0bb
cristian f <licagua@gmail.com>
2021-01-22 20:12:56 +0000

    modificacion consulta turno

--- backend-lacasona/controllers/turno.js
+++ backend-lacasona/controllers/turno.js
@@ -312,8 +312,10 @@ exports.getFechasTurnosSegunProfesional = async (req, res) =>{
 
 exports.getTurnosFechaTipo = async (req, res) =>{
     if(req.params.tipo == 0){
-		await pool.query ('SELECT t.id_turno, t.id_tipo_turno, t.costo_base, t.estado, t.fecha, t.hora,t.id_profesional, t.id_paciente, t.id_tipo_turno, t.observacion, t.profesional_disponible, t.turno_tratamiento'+ 
+		await pool.query ('SELECT p.nombre, p.apellido, p.email, p.telefono, t.id_turno, t.id_tipo_turno, t.costo_base, t.estado, t.fecha, t.hora,t.id_profesional, t.id_paciente, t.id_tipo_turno, t.observacion, t.profesional_disponible, t.turno_tratamiento'+ 
 					' FROM turno as t' +
+					' INNER JOIN entrevista AS e ON e.id_entrevista = t.id_tipo_turno ' + 
+					' INNER JOIN persona AS p ON p.id_persona = e.id_persona ' + 
 					' WHERE t.fecha ="'+req.params.fecha+'" AND t.turno_tratamiento="'+req.params.tipo+'" ' ,function(err,sql){
 			if(err){
 				console.log(err);
