536773b66fce3b23b49ce3ffb09a0d8959babbb4
cristian f <licagua@gmail.com>
2021-02-07 14:12:24 +0000

    .

--- backend-lacasona/controllers/turno.js
+++ backend-lacasona/controllers/turno.js
@@ -357,7 +357,7 @@ exports.getTurnosFechaTipo = async (req, res) =>{
     if(req.params.tipo == 0){
 		await pool.query ('SELECT t.id_turno, t.id_tipo_turno, t.costo_base, t.estado, t.fecha, t.hora,t.id_profesional, t.id_paciente, t.id_tipo_turno, t.observacion, t.profesional_disponible, t.turno_tratamiento'+ 
 					' FROM turno as t' +
-					' WHERE t.fecha ="'+req.params.fecha+'" AND t.turno_tratamiento="'+req.params.tipo+'" ' ,function(err,sql){
+					' WHERE t.fecha ="'+req.params.fecha+'" AND t.turno_tratamiento="'+req.params.tipo+'" order by hora' ,function(err,sql){
 			if(err){
 				console.log(err);
 				res.status(400).json({
