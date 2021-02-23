d4e0ee2e7ec56cd4de37f5bd3bd98e51b8c83120
cristian f <licagua@gmail.com>
2021-02-23 20:35:49 +0000

    .

--- backend-lacasona/router/historia_clinica.js
+++ backend-lacasona/router/historia_clinica.js
@@ -12,6 +12,6 @@ router.get('/getHCTratamientoSinFechaAlta',ctrlHC.getHCTratamientoSinFechaAlta);
 router.get('/getHC/:idHC',ctrlHC.getHC);
 router.get('/getHCTratamiento/:id',ctrlHC.getHCTratamientoId);
 router.get('/leerHC/:id',ctrlHC.leerhc);
-router.get('/getEvolucion/:idHC/:fecha',ctrlHC.getEvolucionHCFecha);
+router.get('/getEvolucion/:id_hc_tratamiento/:fecha',ctrlHC.getEvolucionHCFecha);
 
 module.exports = router;
