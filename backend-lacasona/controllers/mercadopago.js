496ddd22478b5fd6708280ae247b9054f252da15
unknown <analia.f93@gmail.com>
2021-03-14 12:06:10 -0300

    cambios

--- backend-lacasona/controllers/mercadopago.js
+++ backend-lacasona/controllers/mercadopago.js
@@ -66,6 +66,8 @@ exports.confirmarPago = (req, res, next) => {
     const nombreusuario = reqParams.nombre   //el que colocamos en la notification_url del objeto preference
     //const idDelProducto = reqParams.idDelProducto //IDEM
     console.log('usuario   ',nombreusuario);
+    console.log("imprimir que viene"+req.reqBody);
+
     const paymentId = reqBody.data.id //identifación del pago
   
     await obtenerInfoDePago(paymentId)
