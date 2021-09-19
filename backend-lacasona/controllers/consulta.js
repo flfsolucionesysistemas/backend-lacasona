9f49b0d4dd0c5e761c623ad94a768ffabedc604e
cristian f <licagua@gmail.com>
2021-09-19 20:33:40 +0000

    zoom cdf 3

--- backend-lacasona/controllers/consulta.js
+++ backend-lacasona/controllers/consulta.js
@@ -74,7 +74,9 @@ exports.addConsulta= async (req, res) =>{
 						url: conex.host+conex.port+'/turno/addMeeting/289',
 						method: 'get'
 					});
-					console.log("datos zoom  "+datos_zoom);
+					console.log("datos zoom  "+ JSON.stringify(datos_zoom));
+					
+
 					//console.log("datos zoom  "+datos_zoom.data[0].join_url);
 					//seteo el join_url para enviar por email
 					meet=datos_zoom.data[0].join_url;	
